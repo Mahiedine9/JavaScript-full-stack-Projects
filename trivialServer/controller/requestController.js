f4cd7c9c68c5acc0b7b6ef06114f99ac8c77fff4
Ferdjoukh Mahiedine <mahiedine.ferdjoukh.etu@a10p7.fil.univ-lille.fr>
2024-01-16 12:53:26 +0100

    construction de lentete de la requete

--- trivialServer/controller/requestController.js
+++ trivialServer/controller/requestController.js
@@ -14,8 +14,44 @@ export default class ResquestController{
     }
 
     handleRequest() {
-        this.prepareResponse();
-        this.buildResponse();
+        try {
+            this.prepareResponse();
+            this.buildResponse();
+            
+        } catch (error) {
+            this.handleInvalidRequest();
+        }
+
+    }
+
+
+    prepareResponse() {
+        this.response.statusCode = 200;
+        this.response.setHeader( 'Content-Type' , contentType);
+        if (contentType == 'text/html'){
+            this.response.write('<html>');
+            this.response.write('<head></head>');
+            this.response.write('<body>');
+            this.response.write('<h1>Hello, World!</h1>');
+            this.response.write('</body>');
+            this.response.write('</html>');
+        } else if (contentType === 'application/json') {
+            // Gérer le contenu JSON si nécessaire
+            const jsonData = { message: 'Hello, World!' };
+            this.response.write(JSON.stringify(jsonData));
+        }
+    }
+    
+    handleInvalidRequest() {
+        this.response.statusCode = 404;
+        this.response.setHeader( 'Content-Type' , text/plain);
+        this.response.write('Invalid Request');
+        this.response.end();
+
+    }
+
+    buildResponse(){
+  
     }
     
 
