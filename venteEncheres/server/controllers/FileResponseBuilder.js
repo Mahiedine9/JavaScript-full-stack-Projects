d8bd0600ab7cfa66bd9d5f2c1547fb8e1f5f3b79
Ferdjoukh Mahiedine <mahiedine.ferdjoukh.etu@a11p26.fil.univ-lille.fr>
2024-02-07 01:32:57 +0100

    update des routes

---
+++ venteEncheres/server/controllers/FileResponseBuilder.js
@@ -0,0 +1,28 @@
+const responseBuilder = require("./responseBuilder");
+const { readFileSync, accessSync } = require('fs');
+
+
+class FileResponseBuilder extends responseBuilder {
+    constructor(request, response, statusCode, contentType) {
+        super(request, response, statusCode, contentType);
+    }
+
+    handleRequest() {
+        this.buildBody();
+        this.response.end();
+    }
+
+    buildBody() {
+        const fullPath = `.${this.getUrl().pathname}`;
+        try {
+            accessSync(fullPath);
+            const content = readFileSync(fullPath);
+            this.response.write(content);
+        } catch (error) {
+            this.response.statusCode = 404;
+            this.response.write('Resource not found');
+        }
+    }
+}
+
+module.exports = FileResponseBuilder;
--- venteEncheres/server/controllers/responseController.js
+++ venteEncheres/server/controllers/responseController.js
@@ -3,50 +3,46 @@ const InvalideResponse = require('./InvalideResponse');
 const auctioneer = require('./auctioneer');
 const home = require('./home');
 const bidder = require('./bidder');
+const FileResponseBuilder = require('./FileResponseBuilder');
+const { getContentTypeFrom } = require('./mimeTypes');
 
 
 
-class responseController{
-
+class responseController {
     #responseBuilder;
     #url;
     #request;
     #response;
 
-
-    constructor(url, request, response){
+    constructor(url, request, response) {
         this.#url = url;
         this.#request = request;
         this.#response = response;
     }
 
-    handleRequest(){
-        if (this.#url == '/' ){
+    handleRequest() {
+        const contentType = getContentTypeFrom(this.#url);
+        
+        if (this.#url == '/') {
             this.#responseBuilder = new home(this.#request, this.#response, 200, 'text/html');
-        } 
-
-        else if (this.#url == '/about') {
+        } else if (this.#url == '/about') {
             this.#responseBuilder = new about(this.#request, this.#response, 200, 'text/html');
-        }
-
-        else if (this.#url == '/auctioneer') {
+        } else if (this.#url == '/auctioneer') {
             this.#responseBuilder = new auctioneer(this.#request, this.#response, 200, 'text/html');
-        }
-
-        else if (this.#url == '/bidder') {
+        } else if (this.#url == '/bidder') {
             this.#responseBuilder = new bidder(this.#request, this.#response, 200, 'text/html');
-        }
-        
-        else{
+        } else if (this.#url.startsWith('/public')) {
+            
+            this.#responseBuilder = new FileResponseBuilder(this.#request, this.#response, 200, contentType);
+        } else if (this.#url.startsWith('/scripts')) {
+            const newUrl = '/public' + this.#url;
+            this.#responseBuilder = new FileResponseBuilder(this.#request, this.#response, 200, contentType);
+        } else {
             this.#responseBuilder = new InvalideResponse(this.#request, this.#response, 200, 'text/html');
-        } 
+        }
 
-        
-
-        
-        
-
-        this.#responseBuilder.handleRequest();      
+        this.#responseBuilder.handleRequest();
     }
 }
+
 module.exports = responseController;
