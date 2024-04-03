4dd1719af26ed9365b4c69a919117f97feacb6b5
mahiedine.ferdjoukh.etu <mahiedine.ferdjoukh.etu@univ-lille.fr>
2024-04-03 22:28:06 +0200

    dernieres modifications

--- Spectacle/server/controllers/admin.controller.js
+++ Spectacle/server/controllers/admin.controller.js
@@ -2,15 +2,7 @@ const User = require('../models/user.model').model;
 
 
 
-module.exports.adminPage2 = async (req, res) => {
-    try {
-      res.redirect('/admin.html');
-    } catch (error) {
-      console.error(`Erreur lors de l'accès à la page d'administration : ${error.message}`);
-      res.status(500).json({ message: 'Erreur serveur lors de l\'accès à la page d\'administration' });
-    }
-    
-};
+
 
 
 module.exports.adminPage =
